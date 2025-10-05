a113fe491f2136124f570cdf8318f31ac088e2a2
alaaAdel <alaaward3000@gmail.com>
2025-10-05 09:49:04 +0300

    make some modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import Head from './components/Head.jsx'
-import Header from './components/Header.jsx';
-import Hero from './components/Hero.jsx'
-import Footer from './components/Footer.jsx'
-import Flash from './components/Flash.jsx';
-import BrowseCategories from './components/BrowseCategories.jsx';
-import BestSelling from './components/BestSelling.jsx';
-import ExploreProduct from './components/ExploreProduct.jsx'
-import Music from './components/music.jsx';
-import PromotionalGrid from './components/PromotionalGrid.jsx'
-import FeaturesSection from './components/FeaturesSection.jsx'
+import Home from './Home.jsx'
+
 const App = () => {
   return (
    <>
-   <Head />
-   <Header />
-   <Hero />
+   <Home />
    
-   <Flash />
-   <BrowseCategories />
-   <BestSelling />
-   <Music />
-   <ExploreProduct />
-   <PromotionalGrid />
-   <FeaturesSection />
-   <Footer />
+  
 
    </>
   )
---
+++ src/Home.jsx
@@ -0,0 +1,34 @@
+
+
+import React from 'react';
+import Head from './components/Head.jsx'
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx'
+import Footer from './components/Footer.jsx'
+import Flash from './components/Flash.jsx';
+import BrowseCategories from './components/BrowseCategories.jsx';
+import BestSelling from './components/BestSelling.jsx';
+import ExploreProduct from './components/ExploreProduct.jsx'
+import Music from './components/Music.jsx';
+import PromotionalGrid from './components/PromotionalGrid.jsx'
+import FeaturesSection from './components/FeaturesSection.jsx'
+const Home = () => {
+  return (
+   <>
+   <Head />
+   <Header />
+   <Hero />
+   <Flash />
+   <BrowseCategories />
+   <BestSelling />
+   <Music />
+   <ExploreProduct />
+   <PromotionalGrid />
+   <FeaturesSection />
+   <Footer />
+
+   </>
+  )
+}
+
+export default Home
